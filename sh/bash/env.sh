1c08bf1ed0745699fd02a9197d4d5e4e24db381f
tomoya-k31 <chacogre@gmail.com>
2018-08-23 09:54:41 +0900

    add plenv

--- sh/bash/env.sh
+++ sh/bash/env.sh
@@ -2,6 +2,9 @@
 # ruby - bash
 eval "$(rbenv init -)"
 
+# perl
+eval "$(plenv init -)"
+
 # nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
--- sh/zsh/env.sh
+++ sh/zsh/env.sh
@@ -5,6 +5,9 @@ eval "$(rbenv init - zsh)"
 # direnv
 eval "$(direnv hook zsh)"
 
+# perl
+eval "$(plenv init - zsh)"
+
 # nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
