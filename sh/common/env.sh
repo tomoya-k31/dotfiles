eec339a46cd5c3241d270561da887d4041eabd3f
tomoya-k31 <chacogre@gmail.com>
2022-04-18 22:16:39 +0900

    poetry追加

--- sh/common/env.sh
+++ sh/common/env.sh
@@ -23,6 +23,8 @@ PERL_MM_OPT="INSTALL_BASE=${HOME}/perl5"; export PERL_MM_OPT;
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+## Poetry
+export PATH="$HOME/.poetry/bin:$PATH"
 
 ## for aws-sam-local
 USER_BASE_PATH=$(python -m site --user-base)
