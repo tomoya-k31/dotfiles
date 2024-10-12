84e9be228830a0fa8ab39a97d9661e4ad82ba201
tomoya-k31 <chacogre@gmail.com>
2024-10-13 05:31:04 +0900

    add krew alias

--- sh/common/alias.sh
+++ sh/common/alias.sh
@@ -22,8 +22,18 @@ alias now="date '+%Y%m%d-%H%M%S'"
 
 # kubernetes
 # https://kubernetes.io/ja/docs/tasks/tools/install-kubectl/
+alias kubectl=kubecolor
 alias k=kubectl
+# krew
+alias kx='kubectl ctx'
+alias kn='kubectl ns'
+alias stern='kubectl stern'
+alias rolesum='kubectl rolesum'
+alias rakkess='kubectl access-matrix'
 
 # https://kubernetes.io/docs/reference/kubectl/generated/kubectl_completion/
 source <(kubectl completion zsh)
 kubectl completion zsh > "${fpath[1]}/_kubectl"
+
+# Kubecolor https://kubecolor.github.io/setup/shells/zsh/
+compdef kubecolor=kubectl
