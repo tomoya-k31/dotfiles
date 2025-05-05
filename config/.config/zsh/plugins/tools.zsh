a6a5db9e68a6f9048405a1669ccb3cdaac70e774
tomoya-k31 <chacogre@gmail.com>
2025-05-05 17:04:06 +0900

    Add fzf history widget for reverse search with up arrow key

--- config/.config/zsh/plugins/tools.zsh
+++ config/.config/zsh/plugins/tools.zsh
@@ -2,3 +2,15 @@
 
 # Load 1Password plugins
 source ~/.config/op/plugins.sh
+
+
+# 上矢印キーでfzfによる逆順履歴検索を起動
+function fzf-history-widget() {
+  local selected=$(tac $HISTFILE | sed 's/^: [0-9]*:[0-9]*;//' | fzf --query="$LBUFFER" --prompt="History> ")
+  if [[ -n $selected ]]; then
+    LBUFFER=$selected
+    zle redisplay
+  fi
+}
+zle -N fzf-history-widget
+bindkey '^[[A' fzf-history-widget
